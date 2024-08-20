d202f1cabd8161342940b9174be4412482d2d4e5
Henriqueamfreitas <henriqueannicchino89@gmail.com>
2024-08-20 17:12:51 -0300

    documentação

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,13 @@ import "express-async-errors";
 import express, { Application, json } from "express";
 import { userRouters, medicoRouters, hospitalRouters, chamadoRouters } from "./routers";
 import middlewares from "./middlewares";
+import swaggerUi from "swagger-ui-express"
+import swaggerDocs from "../src/swagger.json"
 
 const app: Application = express();
 app.use(json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use('/login', userRouters.userRouter)
 app.use('/medicos', medicoRouters.medicoRouter)
 app.use('/hospital', hospitalRouters.hospitalRouter)
