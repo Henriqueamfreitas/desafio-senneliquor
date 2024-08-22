11b237488fad46092bcef3ecb4809ce0cf5e5aa7
Henriqueamfreitas <henriqueannicchino89@gmail.com>
2024-08-21 21:45:27 -0300

    feat: adicionando PATCH no CORS permitido

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,20 +8,19 @@ import swaggerDocs from "../src/swagger.json";
 
 const app: Application = express();
 
-// Configuração do CORS
 app.use(cors({
-  origin: 'http://localhost:5173', // Substitua com a URL do seu frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Adapte conforme necessário
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], 
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 app.use(json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-app.use('/login', userRouters.userRouter);
-app.use('/medicos', medicoRouters.medicoRouter);
-app.use('/hospital', hospitalRouters.hospitalRouter);
-app.use('/chamados', chamadoRouters.chamadoRouter);
+app.use("/login", userRouters.userRouter);
+app.use("/medicos", medicoRouters.medicoRouter);
+app.use("/hospital", hospitalRouters.hospitalRouter);
+app.use("/chamados", chamadoRouters.chamadoRouter);
 
 app.use(middlewares.handleError);
 
