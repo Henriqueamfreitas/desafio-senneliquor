60ad016e4d39866c561b605e7bfb70971f6b8b1e
Henriqueamfreitas <henriqueannicchino89@gmail.com>
2024-08-23 09:27:05 -0300

    feat: ajustes CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ import swaggerDocs from "../src/swagger.json";
 const app: Application = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://frontend-chi-two-72.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], 
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
